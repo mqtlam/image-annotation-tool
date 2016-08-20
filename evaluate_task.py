f50a0f0c028db7f37719c6ee1b2cec58a6e4e2f7
Michael Lam <lamm@eecs.oregonstate.edu>
2016-08-20 01:34:25 +0000

    Add basic evaluation script

---
+++ evaluate_task.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+"""
+Evaluate an annotation task.
+"""
+
+import os
+import argparse
+import json
+import lmdb
+
+def get_classes(args):
+    """Get list of classes.
+
+    Args:
+        args: command line args
+
+    Returns:
+        list of classes
+    """
+    tasks_json = os.path.join('data', 'tasks.json')
+    with open(tasks_json, 'r') as f:
+        data = json.load(f)
+    return data[args.task_id]['classes']
+
+def main():
+    parser = argparse.ArgumentParser(description='Evaluate task.')
+    parser.add_argument('task_id', type=str, help='task id')
+    parser.add_argument('--correct_class', type=int, default=0, help='correct class index')
+    parser.add_argument('--plausible_class', type=int, default=1, help='plausible class index')
+    parser.add_argument('--incorrect_class', type=int, default=2, help='incorrect class index')
+    args = parser.parse_args()
+
+    # set up data and counters
+    classes = get_classes(args)
+    class_counter = {}
+    for i in range(len(classes)):
+       class_counter[i] = 0
+    total_counter = 0
+
+    # read from lmdb
+    db_path = os.path.join('data', args.task_id, 'annotations')
+    with lmdb.open(db_path, map_size=pow(2, 40)) as env:
+        with env.begin(write=False) as txn:
+            cursor = txn.cursor()
+            for key, value in cursor:
+                value = int(value)
+                if value not in class_counter:
+                    raise ValueError('encountered unexpected label {0} for key {1}'.format(value, key))
+                class_counter[value] += 1
+                total_counter += 1
+
+    # print results
+    print "Num correct: {0}".format(class_counter[args.correct_class])
+    print "Num plausible: {0}".format(class_counter[args.plausible_class])
+    print "Num incorrect: {0}".format(class_counter[args.incorrect_class])
+    print "Total: {0}".format(total_counter)
+
+main()
